0032be4378290435727bd576841b224e8b01e7e3
bartektelec <bartektelec@gmail.com>
2020-12-20 13:59:35 +0100

    feat: :tada: initiate expo app with native-base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { Container, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'blue',
     justifyContent: 'center',
+    alignContent: 'center',
+    flex: 1,
+  },
+  text: {
+    color: 'red',
+    backgroundColor: 'pink',
   },
 });
+
+export default function App() {
+  const [isReady, setReady] = useState(false);
+  useEffect(() => {
+    async function load() {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        ...Ionicons.font,
+      });
+      setReady(true);
+    }
+
+    load();
+  }, []);
+
+  if (!isReady) return <AppLoading />;
+
+  return (
+    <Container style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+    </Container>
+  );
+}
