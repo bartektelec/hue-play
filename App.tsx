ffe88d54c1dfcad91a13819aae84a9e564d7184f
bartektelec <bartektelec@gmail.com>
2020-12-21 14:53:08 +0100

    feat: :sparkles: print out available bridges on network

--- App.tsx
+++ App.tsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { StyleSheet } from 'react-native';
 import AppLoading from 'expo-app-loading';
-import { Container, Text } from 'native-base';
+import { Container, List, ListItem, Text } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 
+interface HueBridge {
+  id: string;
+  internalipaddress: string;
+}
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'blue',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignContent: 'center',
-    flex: 1,
-  },
-  text: {
-    color: 'red',
-    backgroundColor: 'pink',
   },
 });
 
 export default function App() {
   const [isReady, setReady] = useState(false);
+  const [bridgelist, setBridgelist] = useState<HueBridge[]>([]);
+  const [connected, setConnected] = useState<string | null>(null);
+
+  const handleBridgeConnect = useCallback((ip: string) => {
+    setConnected(ip);
+  }, []);
   useEffect(() => {
-    async function load() {
+    async function loadFonts() {
       await Font.loadAsync({
         Roboto: require('native-base/Fonts/Roboto.ttf'),
         Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
@@ -30,16 +35,32 @@ export default function App() {
       setReady(true);
     }
 
-    load();
+    async function searchBridges() {
+      const response = await fetch('https://discovery.meethue.com/');
+      const result = await response.json();
+      setBridgelist(result);
+    }
+
+    loadFonts();
+    searchBridges();
   }, []);
 
   if (!isReady) return <AppLoading />;
 
   return (
     <Container style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
+      <List>
+        {bridgelist.map(bridge => (
+          <ListItem
+            onPress={() => handleBridgeConnect(bridge.internalipaddress)}
+          >
+            <Text>
+              {bridge.id} - {bridge.internalipaddress}
+            </Text>
+          </ListItem>
+        ))}
+      </List>
+      {connected ? <Text>Connected to {connected}</Text> : null}
     </Container>
   );
 }
